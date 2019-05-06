d71fd9ad72aec8faf989dcd00b6a0f0bfba6bf44
EgoSpeed <44160919+EgoSpeed@users.noreply.github.com>
2019-05-06 20:39:03 +0100

    1st push, created main and started to create menu

---
+++ DataDrivenApp/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Header.h"
+
+void scrollingMenu()
+
+{
+	string scrollingMenu[] =
+		("Count the number of tweets in the file", "Count the number of tweets that mention money", "Count the number of tweets relation to politics",
+			"Display all the tweets mentioning Paris", "Display all the tweets mentioning DreamWorks", "Display all the tweets mentioning Car", "Display all the tweets mentioning Garden"
+			"Display all the tweets mentioning House", "Display all the tweets mentioning Life");
+
+	while (true)
+
+}
