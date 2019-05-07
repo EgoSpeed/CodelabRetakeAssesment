0ef989b7b0ecba6e0132b782487ffa4cc50e387f
EgoSpeed <44160919+EgoSpeed@users.noreply.github.com>
2019-05-07 12:40:14 +0100

    Forgot to push to orgin..

--- DataDrivenApp/Source.cpp
+++ DataDrivenApp/Source.cpp
@@ -9,16 +9,16 @@ using namespace std;
  //Creating menu value using int
 int scrollingMenu()
 {
-	string line; 
+	string line;
 	int phrase;
 	ifstream inFile;
-	
+
 	inFile.open("SampleTweets.csv"); //Opens File
 
-	string scrollingMenu[9] =//Name of the string scrolling data
+	string scrollingMenu[8] =//Name of the string scrolling data
 	{ "Count the number of tweets in the file", "Count the number of tweets that mention money", "Count the number of tweets relation to politics",
-			"Display all the tweets mentioning Paris", "Display all the tweets mentioning DreamWorks", "Display all the tweets mentioning Car", "Display all the tweets mentioning Garden", 
-			"Display all the tweets mentioning House", "Display all the tweets mentioning Life",};
+			"Display all the tweets mentioning Paris", "Display all the tweets mentioning DreamWorks", "Display all the tweets mentioning Car", "Display all the tweets mentioning Garden",
+			"Display all the tweets mentioning House", "Display all the tweets mentioning Life", };
 
 	int pointer = 0;
 	int answerTotal;
@@ -66,22 +66,24 @@ int scrollingMenu()
 				break;
 			}
 
-	
-			
+
+
 			else if (GetAsyncKeyState(VK_RETURN) != 0) //Uses the enter key on different cases which are the strings above asking the questions to be inputted
 			{
 				switch (pointer)
 				{
 				case 0: //Case for number of tweets to be opened
 
-					if (inFile.good()) { 
+					if (inFile.good()) {
 						cout << "File being read..." << endl;
 						while (!inFile.eof()) {
-							getline(inFile, line);
-							answerTotal++; //Stores the number
-							cout << "Total number of tweets is: " << answerTotal << endl; //Provides the text including the stored number 
-							Sleep(10000);
-						} break;
+								getline(inFile, line);
+								answerTotal++; //Stores the number
+							 //Provides the text including the stored number 
+						} cout << "Total number of tweets is: " << answerTotal << endl;
+						Sleep(10000);
+						break;
+					}
 
 				case 1:
 
@@ -90,26 +92,111 @@ int scrollingMenu()
 						while (getline(inFile, line)) {
 							if ((phrase = line.find("money", 0  /*finds money text in sampleTweets/csv file*/)) != string::npos || (phrase = line.find("Money", /*Second time is for any capitlisation used in the code to provide certain result */ 0))
 								!= string::npos) { //Npos allows the static value to be it's greatest value for an element of type size
-								answerTotal++; 
+								answerTotal++;
 							}
 
-					} break;
+						}
+					}
+					cout << "Total number of money mentioned tweets is: " << answerTotal << endl;
+					Sleep(10000);
+					break;
 
 				case 2:
 					if (inFile.good()) {
 						cout << "File being read..." << endl;
 						while (getline(inFile, line)) {
+							if ((phrase = line.find("Politics", 0)) != string::npos || ((phrase = line.find("Government", 0)) != string::npos || ((phrase = line.find("Brexit", 0)) != string::npos))) {
+								answerTotal++;
+							}
+						}
 
-				
+
+					} cout << "Total number of political tweets is: " << answerTotal << endl;
+					Sleep(10000);
+					break;
+
+				case 3:
+					if (inFile.good()) {
+						cout << "File being read..." << endl;
+						while (getline(inFile, line)) {
+							if ((phrase = line.find("Paris", 0)) != string::npos || ((phrase = line.find("paris", 0)) != string::npos || ((phrase = line.find("PARIS", 0)) != string::npos))) {
+								answerTotal++;
+							}
+							cout << "Here are tweets relating to paris" << line << endl;
+							Sleep(10000);
+						}
+
+
+					} break;
+
+				case 4:
+					if (inFile.good()) {
+						cout << "File being read..." << endl;
+						while (getline(inFile, line)) {
+							if ((phrase = line.find("Dreamworks", 0)) != string::npos || ((phrase = line.find("dreamworks", 0)) != string::npos || ((phrase = line.find("DREAMWORK", 0)) != string::npos))) {
+								answerTotal++;
+							}
+							cout << "Here are tweets relating to Dreamworks" << line << endl;
+							Sleep(10000);
+						}
+
+					}break;
+
+				case 5:
+					if (inFile.good()) {
+						cout << "File being read..." << endl;
+						while (getline(inFile, line)) {
+							if ((phrase = line.find("Car", 0)) != string::npos || ((phrase = line.find("car", 0)) != string::npos || ((phrase = line.find("CAR", 0)) != string::npos))) {
+								answerTotal++;
+							}
+							cout << "Here are tweets relating to Car" << line << endl;
+							Sleep(10000);
+						}
+
+					}break;
+
+				case 6:
+					if (inFile.good()) {
+						cout << "File being read..." << endl;
+						while (getline(inFile, line)) {
+							if ((phrase = line.find("Garden", 0)) != string::npos || ((phrase = line.find("garden", 0)) != string::npos || ((phrase = line.find("GARDEN", 0)) != string::npos))) {
+								answerTotal++;
+							}
+							cout << "Here are tweets relating to Garden" << line << endl;
+							Sleep(10000);
+						}
+
+					}break;
+				case 7:
+					if (inFile.good()) {
+						cout << "File being read..." << endl;
+						while (getline(inFile, line)) {
+							if ((phrase = line.find("House", 0)) != string::npos || ((phrase = line.find("house", 0)) != string::npos || ((phrase = line.find("HOUSE", 0)) != string::npos))) {
+								answerTotal++;
+							}
+							cout << "Here are tweets relating to House" << line << endl;
+							Sleep(10000);
+						} break;
+					}
+
+				case 8:
+					if (inFile.good()) {
+						cout << "File being read..." << endl;
+						while (getline(inFile, line)) {
+							if ((phrase = line.find("Life", 0)) != string::npos || ((phrase = line.find("life", 0)) != string::npos || ((phrase = line.find("LIFE", 0)) != string::npos))) {
+								answerTotal++;
+							}
+							cout << "Here are tweets relating to Life" << line << endl;
+							Sleep(10000); //Forgot to add that this allows terminal to view the message to be read for 10000 miliseconds
+						}
+
+
+					}break;
 				}
 			}
-
-			Sleep(150);
 		}
-
 	}
-		return 0;
+	return 0;
 }
-
 
 
